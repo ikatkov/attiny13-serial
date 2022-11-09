40db019e7a6bda2b5932fe6f661f817f27d99332
ikatkov <ikatkov@gmail.com>
2022-11-09 00:33:27 -0800

    working code

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,16 @@
 int main(void)
 {
 
-        /* setup */
-        DDRB |= (1 << LED_PIN);  // set LED pin as OUTPUT
-        //DDRB |= (1 << PB4);  // set LED pin as OUTPUT
-        PORTB = 0b00000000; // set all pins to LOW
+    /* setup */
+    DDRB |= (1 << LED_PIN); // set LED pin as OUTPUT
+    // DDRB |= (1 << PB4);  // set LED pin as OUTPUT
+    PORTB = 0b00000000; // set all pins to LOW
 
-        /* loop */
-        while (1)
-        {
-                PORTB ^= _BV(LED_PIN); // toggle LED pin
-                //PORTB ^= _BV(PB4); // toggle LED pin
-                _delay_ms(100);
-        }
+    /* loop */
+    while (1)
+    {
+        PORTB ^= _BV(LED_PIN); // toggle LED pin
+        // PORTB ^= _BV(PB4); // toggle LED pin
+        _delay_ms(1000);
+    }
 }
